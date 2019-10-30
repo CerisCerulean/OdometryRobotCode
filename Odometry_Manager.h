4f2b9adc59c758e34a6354378cc8bb722100050c
James Brown <james@impromptu-software.co.uk>
2019-10-30 17:15:15 +0000

    Added register variables to MD25 Class

--- Odometry_Manager.h
+++ Odometry_Manager.h
@@ -27,11 +27,34 @@ public:
 
 private:
 
-	int m_Mode{ 0 };
-	unsigned char m_MD25Address{ 0 };
-	unsigned char m_VoltageRegister{ 0 };
-	unsigned char m_Motor1CurrentRegister{ 0 };
-	unsigned char m_Motor2CurrentRegister{ 0 };
+	int m_Mode{ 1 };
+
+	const unsigned char m_MD25Address{ 0x58 };
+
+	// Memory Registers in MD25 with default values from datasheet
+	const unsigned char m_Speed1Reg{ 0x00 };
+	const unsigned char m_Speed2Reg{ 0x01 };
+
+	const unsigned char m_Enc1aReg{ 0x02 };
+	const unsigned char m_Enc1bReg{ 0x03 };
+	const unsigned char m_Enc1cReg{ 0x04 };
+	const unsigned char m_Enc1dReg{ 0x05 };
+
+	const unsigned char m_Enc2aReg{ 0x06 };
+	const unsigned char m_Enc2bReg{ 0x07 };
+	const unsigned char m_Enc2cReg{ 0x08 };
+	const unsigned char m_Enc2dReg{ 0x09 };
+
+	const unsigned char m_VoltageReg{ 0x0A };
+	const unsigned char m_Motor1CurrentReg{ 0x0B };
+	const unsigned char m_Motor2CurrentReg{ 0x0C };
+
+	const unsigned char m_SoftwareReg{ 0x0D };
+	
+	const unsigned char m_AccelerationReg{ 0x0E };
+
+	const unsigned char m_ModeReg{ 0x10 };
+
 };
 
 
